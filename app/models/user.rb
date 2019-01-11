074c6a895c256502b7703abce681b8fc4c0f7935
Asel Kenzhebekova <aseliyaaseliya@gmail.com>
2019-01-11 21:39:27 +0600

    Настроила Devise, добавила валидацию к модели Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 	validates :name, presence: true, length: { maximum: 50 }
+	
+	after_create :set_role
+	def set_role
+		user = User.last
+		user.role = 2 if Student.find_by(email: user.email)
+		user.save
+	end
 end
