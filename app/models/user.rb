19b21535233ca3d8cb6fc04ecd633e1e774f62ae
Asel Kenzhebekova <aseliyaaseliya@gmail.com>
2019-01-12 20:15:29 +0600

    Тикет 4 выполнен

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable and :omniauthable
   devise :database_authenticatable, :registerable, :timeoutable, :trackable,
          :recoverable, :rememberable, :validatable
-	
-	after_create :set_role
-	def set_role
-		user = User.last
-		user.role = 2 if Student.find_by(email: user.email)
-		user.save
-	end
 end
