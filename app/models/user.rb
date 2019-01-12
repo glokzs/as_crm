eab4c4183a8d64be01cefdb5888901cea9f78bf6
Asel Kenzhebekova <aseliyaaseliya@gmail.com>
2019-01-12 15:25:22 +0600

    #14 тикет Метод автоопредления роли пользователя внесен в модель User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable and :omniauthable
   devise :database_authenticatable, :registerable, :timeoutable, :trackable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
+  #на данный момент не реализована модель Student
+  after_create :set_role
+  def set_role
+  	user = User.last
+	# user.role = 2 if Student.find_by(email: user.email)
+	user.save
+	end
 end
