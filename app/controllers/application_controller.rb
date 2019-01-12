899ed0c19960959f3bdb20082a6cdf387ac578cd
Asel Kenzhebekova <aseliyaaseliya@gmail.com>
2019-01-12 18:41:47 +0600

    #13 Создала редирект пользователя в Application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+	def after_sign_in_path_for(resource)
+		if current_user.role == 1
+			return admin_users_url
+		elsif current_user.role == 2 && Student.find_by(email: current_user.email)
+			return student_url(current_user)
+		else
+			return root_url
+		end
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,14 @@
 class User < ApplicationRecord
-  
-
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable and :omniauthable
   devise :database_authenticatable, :registerable, :timeoutable, :trackable,
-<<<<<<< HEAD
   :recoverable, :rememberable, :validatable
-  #на данный момент не реализована модель Student
+
+  has_one :student, foreign_key: :email
+  
   after_create :set_role
+  
   def set_role
-  	user = User.last
-	# user.role = 2 if Student.find_by(email: user.email)
-	user.save
-	end
-=======
-         :recoverable, :rememberable, :validatable
-
-
-         has_one :student, foreign_key: :email
->>>>>>> ticket-5
+    user = User.last
+    user.role = 2 if Student.find_by(email: current_user.email)
+    user.save
+  end
 end
