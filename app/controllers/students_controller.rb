2839b6e0fe6e3234eead3a92774c93423adc0f66
Asel Kenzhebekova <aseliyaaseliya@gmail.com>
2019-01-15 01:16:08 +0600

    #34 в контроллер Студентов добавлен метод проверки текущего пользователя

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,15 @@
 class StudentsController < ApplicationController
-   
-  def show
-    @student = Student.find(params[:id])
-  end
+	before_action :set_student
+	
+	def show
+		@student = Student.find(params[:id])
+	end
+
+	def set_student
+		if user_signed_in? && current_user.role == 2
+			student_url(current_user)
+		else 
+			redirect_to root_url
+		end
+	end
 end
