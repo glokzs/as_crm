e67123a7bc9c99761ee844b4d0ac127f4a4d97cf
Игорь Федурин <2372356@gmail.com>
2019-01-15 00:26:24 +0600

    Добавил методы обязательной аутентификации студента и проверки роли пользователя для доступа к экшенам студента #34

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,12 @@
 class StudentsController < ApplicationController
-   
+  before_action :authenticate_user!, :is_student?
+
   def show
     @student = Student.find(params[:id])
+  end
+
+  private
+  def is_student?
+    redirect_to root_path if current_user.role != 2
   end
 end
