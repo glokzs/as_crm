c86b7450d97c72c4a8161e7108626c980d0b3042
Asel Kenzhebekova <aseliyaaseliya@gmail.com>
2019-01-15 20:14:27 +0600

    #33 в контроллер Админ добавлен метод проверки текущего пользователя, предоставляющий доступ к экшнам админа

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,3 +1,11 @@
 class Admin::AdminController < ApplicationController
+	before_action :set_admin
 	layout 'admin'
+	def set_admin
+		if user_signed_in? && current_user.role == 1
+			admin_users_url(current_user)
+		else 
+			redirect_to root_url
+		end
+	end
 end
