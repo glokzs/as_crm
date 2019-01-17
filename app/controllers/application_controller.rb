bb4fe108f3c39a39df0c2f0be994bd55fa1a4006
Игорь Федурин <2372356@gmail.com>
2019-01-17 16:22:54 +0600

    #52 исправил адрес админки, которая теперь доступна по адресу /admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 	def after_sign_in_path_for(resource)
 		if current_user.role == 1
-			return admin_users_url
+			return admin_url
 		elsif current_user.role == 2 && Student.find_by(email: current_user.email)
 			return student_url(current_user)
 		else
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
 
   devise_for :users
 	root to: 'welcome#index'
+  get '/admin' => 'admin/users#index'
 	namespace :admin do
 		resources :students
 		resources :users
 	end
-	resources :students,  only: :show 
+	resources :students,  only: :show
 end
