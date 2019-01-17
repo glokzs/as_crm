707052841c22ae5c4591ca6af01b1951ff958341
Elena Nikiforova <elenanikiforova30031979@gmail.com>
2019-01-17 21:14:07 +0600

    #54 Установлен Active storage

--- app/controllers/admin/students_controller.rb
+++ app/controllers/admin/students_controller.rb
@@ -3,4 +3,13 @@ class Admin::StudentsController < Admin::AdminController
   def show
     @student = Student.find(params[:id])
   end
+
+  def upload_picture
+       @student.picture.attach(uploaded_file) if uploaded_file.present?
+end
+
+def uploaded_file
+    params[:student][:pictures]
+end
+
 end
