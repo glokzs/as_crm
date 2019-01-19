77a1f1d3dd4ab87f70873e1ea69a5faf5e763247
Игорь Федурин <2372356@gmail.com>
2019-01-19 17:40:58 +0600

    #25 создал модель Курс с CRUD операциями для Админа

---
+++ app/controllers/admin/courses_controller.rb
@@ -0,0 +1,43 @@
+class Admin::CoursesController < Admin::AdminController
+  def index
+    @courses = Course.all
+  end
+
+  def show
+    @course = Course.find(params[:id])
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(course_params)
+
+    if @course.save
+      redirect_to admin_courses_path
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @course = Course.find(params[:id])
+    if @course.update(course_params)
+      redirect_to admin_path
+    else
+      render 'edit'
+    end
+  end
+
+
+  def destroy
+    Course.destroy(params[:id])
+
+    redirect_to admin_courses_path
+  end
+
+  def course_params
+    params.require(:course).permit(:name)
+  end
+end
