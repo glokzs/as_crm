7875cf86bed89a077724e5c582bc35ffbe532632
Игорь Федурин <2372356@gmail.com>
2019-01-19 16:24:20 +0600

    #61 добавил логотип в папку public/uploads

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2019_01_18_072103) do
 
   create_table "contracts", force: :cascade do |t|
-    t.date "date", default: "2019-01-18"
+    t.date "date", default: "2019-01-19"
     t.string "number"
     t.integer "student_id"
     t.datetime "created_at", null: false
