fdd64d338169637396e36f3bb4c997da4daf2809
Asel Kenzhebekova <aseliyaaseliya@gmail.com>
2019-02-01 20:02:04 +0600

    #94 Настройка cucumber

---
+++ features/step_definitions/students.rb
@@ -0,0 +1,22 @@
+Допустим("залогинен пользователь с email {string} и паролем {string}") do |email, password|
+ visit('/')
+ within('#new_user') do
+   fill_in('Email', with: email)
+   fill_in('Password', with: password)
+   sleep(2)
+   click_button('Войти')
+   sleep(5)
+ end
+end
+
+То("он может создать студента с данными:") do |table|
+   visit('/admin/students/new') do
+     within('#new_student') do
+     fill_in('Фамилия', with: table.hashes[0][:Фамилия])
+     fill_in('Имя', with: table.hashes[0][:Имя])
+     click_button('Создать')
+   end
+  end
+end
+
+
