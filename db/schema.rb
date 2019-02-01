7c68c542e0205012248c93a8900fff070992621c
Asel Kenzhebekova <aseliyaaseliya@gmail.com>
2019-02-02 00:40:51 +0600

    #116 Редактирование поля Подпись в реквизитах договора

--- db/schema.rb
+++ db/schema.rb
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2019_01_30_184456) do
   end
 
   create_table "contracts", force: :cascade do |t|
-    t.date "date", default: "2019-01-31"
+    t.date "date", default: "2019-02-02"
     t.string "number"
     t.integer "student_id"
     t.datetime "created_at", null: false
