1cbdca82d38b242ec20354401b4735326b463544
Aseliya <46473661+Aseliya@users.noreply.github.com>
2019-02-04 19:59:12 +0600

    Update student.rb

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,11 +2,10 @@ class Student < ApplicationRecord
 	has_one :user, foreign_key: :email
   has_one :contract
   belongs_to :group
+  has_and_belongs_to_many :lessons
   has_one_attached :avatar
-  has_many_attached :images
-
   after_create :create_user
-
+  has_many_attached :images
   enum gender: {male: 1, female: 2}
 
   private
@@ -15,3 +14,8 @@ class Student < ApplicationRecord
     user = User.create!(email: student.email, role: 2)
   end
 end
+
+
+
+  
+  
