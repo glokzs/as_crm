e09435e38e55cb844c61106783ecc12bd1cd23be
Asel Kenzhebekova <aseliyaaseliya@gmail.com>
2019-02-16 15:08:43 +0600

    #137 Создание модели Рецензии(review)

---
+++ db/migrate/20190216090016_create_reviews.rb
@@ -0,0 +1,13 @@
+class CreateReviews < ActiveRecord::Migration[5.2]
+  def change
+    create_table :reviews do |t|
+      t.string :title
+      t.text :content
+      t.integer :homework_id
+      t.date :date
+      t.decimal :rate
+
+      t.timestamps
+    end
+  end
+end
