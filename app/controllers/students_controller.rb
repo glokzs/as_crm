57d739ddd453037f96cdd1ad051025ab801f0a7c
Yura Efremov <yuraefremov18228@gmail.com>
2019-02-27 19:00:24 +0600

    # 140 Update views teacher and add review

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,7 +5,6 @@ class StudentsController < ApplicationController
 
   def show
     @student = Student.find(params[:id])
-    @group = Group.find(params[:id])
     @students = @group.students
     @themes = @group.course.themes
     @lessons = @group.lessons
