e217f46635c2cd0bbcdfb58d11abcaa85bdbb6aa
Asel Kenzhebekova <aseliyaaseliya@gmail.com>
2019-03-02 23:06:27 +0600

    #94 Редактирование настроек cucumber

--- features/support/env.rb
+++ features/support/env.rb
@@ -42,12 +42,12 @@ ActionController::Base.allow_rescue = false
 # You may also want to configure DatabaseCleaner to use different strategies for certain features and scenarios.
 # See the DatabaseCleaner documentation for details. Example:
 #
-Before('@no-txn,@selenium,@culerity,@celerity,@javascript') do
-  # { except: [:widgets] } may not do what you expect here
-  # as Cucumber::Rails::Database.javascript_strategy overrides
-  # this setting.
-  DatabaseCleaner.strategy = :truncation
-end
+# Before('@no-txn,@selenium,@culerity,@celerity,@javascript') do
+#   # { except: [:widgets] } may not do what you expect here
+#   # as Cucumber::Rails::Database.javascript_strategy overrides
+#   # this setting.
+#   DatabaseCleaner.strategy = :truncation
+# end
 
 # Before('not @no-txn', 'not @selenium', 'not @culerity', 'not @celerity', 'not @javascript') do
 #   DatabaseCleaner.strategy = :transaction
