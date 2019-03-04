a062a269233de2c1ff0a5d806bdfbaf65631b704
Elena Nikiforova <elenanikiforova30031979@gmail.com>
2019-03-05 02:23:37 +0600

    #124 добавлена возможность просмотра наличия либо отсутсвия домашка

--- db/schema.rb
+++ db/schema.rb
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2019_01_30_184456) do
   end
 
   create_table "contracts", force: :cascade do |t|
-    t.date "date", default: "2019-03-02"
+    t.date "date", default: "2019-03-05"
     t.string "number"
     t.integer "student_id"
     t.datetime "created_at", null: false
