672bbadf8e7d2a691f56f87435bbffc3dab9fc9b
Asel Kenzhebekova <aseliyaaseliya@gmail.com>
2019-03-23 23:49:28 +0600

    #177 решение конфликта с CI. В поле выбора группы для нового студента обозначила существующую группу

--- features/step_definitions/student_create.rb
+++ features/step_definitions/student_create.rb
@@ -5,7 +5,7 @@
       sleep(3)
       fill_in('Email', with: email)
 
-      select('RoR', from:'Группа')
+      select('RoR-2', from:'Группа')
       sleep(1)
       click_button('Создать')
       sleep(3)
