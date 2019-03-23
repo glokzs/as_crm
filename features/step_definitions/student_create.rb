fd5731152c69b2eefde9094a83aaab835fe77417
Asel Kenzhebekova <aseliyaaseliya@gmail.com>
2019-03-24 01:24:44 +0600

    #177 Откатила на рабочую версию(последний рабочий коммит)

--- features/step_definitions/student_create.rb
+++ features/step_definitions/student_create.rb
@@ -1,18 +1,18 @@
 Если("он создаёт студента с данными: имя {string} email {string}") do |first_name, email|
-    visit("students/new")
-    within('#new_student') do
-      fill_in('Имя', with: first_name)
-      sleep(3)
-      fill_in('Email', with: email)
+  visit("students/new")
+  within('#new_student') do
+    fill_in('Имя', with: first_name)
+    sleep(3)
+    fill_in('Email', with: email)
 
-      select('RoR-2', from:'Группа')
-      sleep(1)
-      click_button('Создать')
-      sleep(3)
-    end
+    select('RoR-2', from:'Группа')
+    sleep(1)
+    click_button('Создать')
+    sleep(3)
   end
-  
-  То("студента Иван видно в списке студентов") do
-    visit("students")
-    find('td', text: 'Иван', match: :prefer_exact)
-  end
+end
+
+То("студента Иван видно в списке студентов") do
+  visit("students")
+  find('td', text: 'Иван', match: :prefer_exact)
+end
