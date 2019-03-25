3eefd8e822122cafb29b9c59dcc4460bf7f0c974
Asel Kenzhebekova <aseliyaaseliya@gmail.com>
2019-03-25 17:29:53 +0600

    #177 Написала тест на создание нового преподавателя в приложении

---
+++ features/step_definitions/teacher_create.rb
@@ -0,0 +1,18 @@
+Если("он создаёт преподавателя с данными: имя {string} фамилия {string} email {string}") do |first_name, last_name, email|
+    visit("teachers/new")
+    within('#new_teacher') do
+      fill_in('Имя', with: first_name)
+      sleep(1)
+      fill_in('Фамилия', with: last_name)
+      sleep(1)
+      fill_in('Email', with: email)
+  
+      click_button('Создать')
+      sleep(2)
+    end
+  end
+  
+  То("преподавателя Виктор Александрович видно в списке преподавателей") do
+    visit("teachers")
+    find('td', text: 'Виктор Александрович', match: :prefer_exact)
+  end
