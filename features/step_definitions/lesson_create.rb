f5c2bc3fbca02e1c37d0ef630de1677603e84f68
yura <18228@inbox.ru>
2019-03-28 20:40:42 +0600

    #183 Написал тест  по созданию занятия (lesson) . Тест находится  в зеленой зоне

--- features/step_definitions/lesson_create.rb
+++ features/step_definitions/lesson_create.rb
@@ -1,19 +1,23 @@
- # Если("он создаёт занятие с данными: название {integer}") do |theme_id|
- #   visit("/admin/lessons/new")
- #   within('#new_lesson') do
- #     select('Массивы, обьекты, события', from:'Тема занятия')
- #     sleep(3)
- #     select('RoR-1', from:'Группа')
- #     sleep(3)
- #     fill_in('Раздаточный материал домашки', with: homework_task)
- #     sleep(7)
- #     click_button('Создать')
- #     sleep(3)
- #   end
- # end
+Если("он создаёт занятие с данными: название {string}") do |string|
+  visit("/admin/lessons/new")
+  within('#new_lesson') do
+    select('ООП', from:'Тема занятия')
+    sleep(7)
+    select('RoR-1', from:'Группа')
+    sleep(7)
+    fill_in('Дата дедлайна', with: '2019-03-05'  )
+    sleep(3)
+    click_button('Создать')
+    sleep(3)
+  end
+end
 
- #  To("занятие Массивы, обьекты, события видно в списке тем") do
- #   visit("/admin/lessons")
- #   find('td', text: 'Массивы, обьекты, события', match: :prefer_exact)
- #   sleep(3)
- # end
+Если("Раздаточный материал домашки {string}") do |string|
+ 
+end
+
+То("занятие ООП видно в списке занятий") do
+  visit("/admin/lessons")
+  find('td', text: 'ООП', match: :prefer_exact)
+  sleep(3)
+end
